354a62b2c5e0510abcafc66aaa1cde9a86954726
filoghost <filoghost@gmail.com>
2020-07-20 20:32:50 +0200

    Unify copyright notices

--- Plugin/src/main/java/me/filoghost/chestcommands/action/PlaySoundAction.java
+++ Plugin/src/main/java/me/filoghost/chestcommands/action/PlaySoundAction.java
@@ -1,25 +1,24 @@
 /*
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- *  GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
  *
- *  You should have received a copy of the GNU General Public License
- *  along with this program. If not, see <https://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
 package me.filoghost.chestcommands.action;
 
+import me.filoghost.chestcommands.util.Strings;
+import me.filoghost.chestcommands.util.collection.Registry;
 import org.bukkit.ChatColor;
 import org.bukkit.Sound;
 import org.bukkit.entity.Player;
-
-import me.filoghost.chestcommands.util.collection.Registry;
-import me.filoghost.chestcommands.util.Strings;
 
 import java.util.Optional;
 
--- Plugin/src/main/java/me/filoghost/chestcommands/parsing/ActionParser.java
+++ Plugin/src/main/java/me/filoghost/chestcommands/parsing/ActionParser.java
@@ -1,27 +1,38 @@
 /*
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- *  GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
  *
- *  You should have received a copy of the GNU General Public License
- *  along with this program. If not, see <https://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
 package me.filoghost.chestcommands.parsing;
+
+import me.filoghost.chestcommands.ChestCommands;
+import me.filoghost.chestcommands.action.Action;
+import me.filoghost.chestcommands.action.BroadcastAction;
+import me.filoghost.chestcommands.action.ChangeServerAction;
+import me.filoghost.chestcommands.action.ConsoleCommandAction;
+import me.filoghost.chestcommands.action.DragonBarAction;
+import me.filoghost.chestcommands.action.GiveItemAction;
+import me.filoghost.chestcommands.action.GiveMoneyAction;
+import me.filoghost.chestcommands.action.OpCommandAction;
+import me.filoghost.chestcommands.action.OpenMenuAction;
+import me.filoghost.chestcommands.action.PlaySoundAction;
+import me.filoghost.chestcommands.action.PlayerCommandAction;
+import me.filoghost.chestcommands.action.SendMessageAction;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
-import me.filoghost.chestcommands.ChestCommands;
-import me.filoghost.chestcommands.action.*;
 
 public class ActionParser {
 
