99aba32af51dc4cefb63860999b667c04ac65bae
filoghost <filoghost@gmail.com>
2020-08-22 14:43:54 +0200

    Add missing header

--- plugin/src/main/java/me/filoghost/chestcommands/parsing/MaterialParser.java
+++ plugin/src/main/java/me/filoghost/chestcommands/parsing/MaterialParser.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright (C) filoghost and contributors
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
 package me.filoghost.chestcommands.parsing;
 
 import me.filoghost.chestcommands.logging.Errors;
